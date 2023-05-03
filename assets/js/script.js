57ddb9b5c17500d4dd51ab0503332ad91b85e057
Stefanie Fisher <stefaniefisher@Stefanies-Air.fios-router.home>
2023-05-02 22:40:22 -0400

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ let instructionSection = document.querySelector('#instructions-section');
 let ingredientSection = document.getElementById('ingredient-section');
 let searchResults = document.querySelector('.search-results');
 const maxResults = 10;
+let searchHistory ;
+
 
 searchButton.addEventListener('click', performSearch);
 
@@ -39,26 +41,10 @@ function renderRecipeButtons(recipes) {
             renderIngredients(recipes[id])});
         searchResults.appendChild(recipeButton); 
         
-        var userInput = document.getElementById("search-input").value;localStorage.setItem("searched", userInput);
-        var userInput = document.getElementById("search-input").value;
-        localStorage.setItem("searched", userInput);
+        // var userInput = document.getElementById("search-input").value;
+        // searchHistory.push(userInput)
+        // localStorage.setItem("searched", JSON.stringify(searchHistory));
 
-    // //   var displaysearchResults = displaysearchResults(searchResultItems);
-
-    //    function displaysearchResults(searchResultItems){
-    //     searchHistory.innterHTML =""
-    //     // for (let i = 0; i< searchResultItems.length; i++){
-    //         const li = document.createElement("li");
-    //         li.textContent = searchResultItems;
-    //         searchResults.appendChild(li);
-
-    //         window.addEventListener('load', function() {
-    //             const searchResultItems = JSON.parse(localStorage.getItem("searchResults")) || [];
-    //             displaySearchResults(searchResultItems);
-    //           });
-    //     // }
-    //    }
-        // JSON.parse(localStorage.getItem("searchResults"));
       
         recipeButton.addEventListener('click', function () {
             let id = recipeButton.id.split('-')[2];
@@ -66,31 +52,48 @@ function renderRecipeButtons(recipes) {
         });
         searchResults.appendChild(recipeButton);
     }
+    var userInput = document.getElementById("search-input").value;
+    searchHistory.push(userInput)
+    localStorage.setItem("searched", JSON.stringify(searchHistory));
+    renderSearchHistory()
 };
 
+// function displaySearchResults(searchResultItems){
+//     var searchHistory = document.querySelector("#history-section")
+//     searchHistory.innerHTML = ""
 
-//   var displaysearchResults = displaysearchResults(searchResultItems);
+    // const li = document.createElement("li");
+    // li.textContent = searchResultItems;
+    // searchHistory.appendChild(li);
 
-function displaySearchResults(searchResultItems){
-    var searchHistory= document.querySelector("#history-section")
-searchHistory.innerHTML =""
-// for (let i = 0; i< searchResultItems.length; i++){
-    const li = document.createElement("li");
-    li.textContent = searchResultItems;
-    searchHistory.appendChild(li);
 
-    // window.addEventListener('load', function() {
-    //     const searchResultItems = JSON.parse(localStorage.getItem("searchResults")) || [];
-    //     displaySearchResults(searchResultItems);
-    //   });
-// }
-}
-window.addEventListener('load', function() {
-    // const searchResultItems = JSON.parse(localStorage.getItem("searched")) || [];
-    const searchResultItems = localStorage.getItem("searched");
+// window.addEventListener('DOMContentLoaded', () => {
+function renderSearchHistory() {
+    var searchResultItems = JSON.parse(localStorage.getItem("searched")) || []
+    searchHistory = searchResultItems
+    var searchRecipe = document.querySelector("#search-field")
     console.log(searchResultItems)
-    displaySearchResults(searchResultItems);
-  });
+    console.log(searchRecipe)
+    // if (searchResultItems !== null) {
+    //     searchResultItems.push(searchRecipe)
+    //     localStorage.setItem("searched", JSON.stringify(searchResultItems))
+    //  }
+     var searchHistorySection = document.querySelector("#history-section")
+     searchHistorySection.innerHTML = ""
+     searchResultItems.forEach(search => {
+        var historyElement = document.createElement("button")
+        historyElement.textContent = search;
+        historyElement.dataset.search = search;
+        historyElement.setAttribute("class", "history-results")
+        searchHistorySection.appendChild(historyElement);
+        historyElement.addEventListener("click", (event) => {
+            var searchItem = event.target.dataset.search;
+         document.querySelector (".history-results").value = searchItem
+        })
+     })
+}
+
+renderSearchHistory()
 
 function renderIngredients(recipe) {
     ingredientSection.innerHTML = '';
@@ -100,12 +103,7 @@ function renderIngredients(recipe) {
         ingredientListItem.textContent = ingredient;
         ingredientSection.appendChild(ingredientListItem);
 
-      
-        // localStorage.setItem("searchResults", JSON.stringify(searchResults))
-
-        
-
-    })
+      })
 
     console.log(ingredients);   
     ingredientString = ingredients.join(' '); 
@@ -152,74 +150,74 @@ async function returnNutrition(ingredientString) {
         console.error(error);
     }
 };
-returnNutrition('chicken')
-function renderNutrition(result) {
-    for (let i = 0; i < result.length; i++) {
-        let currentFood = result[i];
-        console.log(currentFood.name);
-        console.log(currentFood.calories);
-        console.log(currentFood.carbs);
-        console.log(currentFood.cholesterol);
-        console.log(currentFood.saturated_fat);
-        console.log(currentFood.fat);
-        console.log(currentFood.sugar);
-        console.log(currentFood.serving_size);
-        console.log(currentFood.sodium);
-        console.log(currentFood.protein);
-        console.log(currentFood.fiber);
-        console.log(currentFood.potassium);
+// returnNutrition(result)
+// function renderNutrition(result) {
+//     for (let i = 0; i < result.length; i++) {
+//         let currentFood = result[i];
+//         console.log(currentFood.name);
+//         console.log(currentFood.calories);
+//         console.log(currentFood.carbs);
+//         console.log(currentFood.cholesterol);
+//         console.log(currentFood.saturated_fat);
+//         console.log(currentFood.fat);
+//         console.log(currentFood.sugar);
+//         console.log(currentFood.serving_size);
+//         console.log(currentFood.sodium);
+//         console.log(currentFood.protein);
+//         console.log(currentFood.fiber);
+//         console.log(currentFood.potassium);
 
-        let cardBody = document.createElement("div");
-        cardBody.setAttribute("class", "card-body");
+//         let cardBody = document.createElement("div");
+//         cardBody.setAttribute("class", "card-body");
 
-        let ingredientName = document.createElement("h2");
-        ingredientName.textContent = `${currentFood.name}`;
-        cardBody.append(ingredientName);
+//         let ingredientName = document.createElement("h2");
+//         ingredientName.textContent = `${currentFood.name}`;
+//         cardBody.append(ingredientName);
 
-        let ingredientCalories = document.createElement("p");
-        ingredientCalories.textContent = `calories: ${currentFood.calories}`;
-        cardBody.append(ingredientCalories);
+//         let ingredientCalories = document.createElement("p");
+//         ingredientCalories.textContent = `calories: ${currentFood.calories}`;
+//         cardBody.append(ingredientCalories);
 
-        let ingredientCarbs = document.createElement("p");
-        ingredientCarbs.textContent = `carbs: ${currentFood.carbs}`;
-        cardBody.append(ingredientCarbs);
+//         let ingredientCarbs = document.createElement("p");
+//         ingredientCarbs.textContent = `carbs: ${currentFood.carbs}`;
+//         cardBody.append(ingredientCarbs);
 
-        let ingredientCholesterol = document.createElement("p");
-        ingredientCholesterol.textContent = `cholesterol: ${currentFood.cholesterol}`;
-        cardBody.append(ingredientCholesterol);
+//         let ingredientCholesterol = document.createElement("p");
+//         ingredientCholesterol.textContent = `cholesterol: ${currentFood.cholesterol}`;
+//         cardBody.append(ingredientCholesterol);
 
-        let ingredientSatFat = document.createElement("p");
-        ingredientSatFat.textContent = `saturated fat: ${currentFood.saturated_fat}`;
-        cardBody.append(ingredientSatFat);
+//         let ingredientSatFat = document.createElement("p");
+//         ingredientSatFat.textContent = `saturated fat: ${currentFood.saturated_fat}`;
+//         cardBody.append(ingredientSatFat);
 
-        let ingredientFat = document.createElement("p");
-        ingredientFat.textContent = `fat: ${currentFood.fat}`;
-        cardBody.append(ingredientFat);
+//         let ingredientFat = document.createElement("p");
+//         ingredientFat.textContent = `fat: ${currentFood.fat}`;
+//         cardBody.append(ingredientFat);
 
-        let ingredientSugar = document.createElement("p");
-        ingredientSugar.textContent = `sugar: ${currentFood.sugar}`;
-        cardBody.append(ingredientSugar);
+//         let ingredientSugar = document.createElement("p");
+//         ingredientSugar.textContent = `sugar: ${currentFood.sugar}`;
+//         cardBody.append(ingredientSugar);
 
-        let ingredientServe = document.createElement("p");
-        ingredientServe.textContent = `serve: ${currentFood.serving_size}`;
-        cardBody.append(ingredientServe);
+//         let ingredientServe = document.createElement("p");
+//         ingredientServe.textContent = `serve: ${currentFood.serving_size}`;
+//         cardBody.append(ingredientServe);
 
-        let ingredientSodium = document.createElement("p");
-        ingredientSodium.textContent = `sodium: ${currentFood.sodium}`;
-        cardBody.append(ingredientSodium);
+//         let ingredientSodium = document.createElement("p");
+//         ingredientSodium.textContent = `sodium: ${currentFood.sodium}`;
+//         cardBody.append(ingredientSodium);
 
-        let ingredientProtein = document.createElement("p");
-        ingredientProtein.textContent = `protein: ${currentFood.protein}`;
-        cardBody.append(ingredientProtein);
+//         let ingredientProtein = document.createElement("p");
+//         ingredientProtein.textContent = `protein: ${currentFood.protein}`;
+//         cardBody.append(ingredientProtein);
 
-        let ingredientFiber = document.createElement("p");
-        ingredientFiber.textContent = `fiber: ${currentFood.fiber}`;
-        cardBody.append(ingredientFiber);
+//         let ingredientFiber = document.createElement("p");
+//         ingredientFiber.textContent = `fiber: ${currentFood.fiber}`;
+//         cardBody.append(ingredientFiber);
 
-        let ingredientPotassium = document.createElement("p");
-        ingredientPotassium.textContent = `potassium: ${currentFood.potassium}`;
-        cardBody.append(ingredientPotassium);
+//         let ingredientPotassium = document.createElement("p");
+//         ingredientPotassium.textContent = `potassium: ${currentFood.potassium}`;
+//         cardBody.append(ingredientPotassium);
 
-        nutritionListItem.append(cardBody);
-    }
-};
+//         nutritionListItem.append(cardBody);
+//     }
+// };
